070de5bcc0252ad22ba3cbf99cd15686929410d2
reshad21 <miakala7@gmail.com>
2022-12-25 11:58:44 +0600

    routes setup complete

---
+++ src/Shared/Navber/Navber.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navber = () => {
+    return (
+        <div>
+            <h1>This is top navber of the movie website</h1>
+        </div>
+    );
+};
+
+export default Navber;
