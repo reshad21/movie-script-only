4d2197e83bf061e650913087b285b2696b34acb5
reshad21 <miakala7@gmail.com>
2023-01-09 22:37:10 +0600

    reactquries added

--- src/Pages/Home/HeroSlider/HeroSlider.jsx
+++ src/Pages/Home/HeroSlider/HeroSlider.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import './HeroSlider.css';
+
 
 const HeroSlider = () => {
+    const [sliders, setSlider] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setSlider(data)
+            })
+    }, [])
+
     return (
         <div>
-            <h1>This is slider section</h1>
+
+            <p className='my-8'>Total length: {sliders.length}</p>
+
+            <div>
+                <Slide className="slide-container">
+                    {sliders.map((slider, index) => (
+                        <div className="each-slide" key={index}>
+                            <div >
+                                <span>{slider.title}</span>
+                                <img src={slider.thumbnailUrl} alt="" />
+                            </div>
+                        </div>
+                    ))}
+                </Slide>
+            </div>
+
+
+
+
+
         </div>
     );
 };
