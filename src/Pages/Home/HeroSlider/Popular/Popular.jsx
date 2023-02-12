70477b9fa48569ccdcea22c6251cb6fb69545fd7
reshad21 <miakala7@gmail.com>
2023-02-13 00:01:50 +0600

    popular card design complete

--- src/Pages/Home/HeroSlider/Popular/Popular.jsx
+++ src/Pages/Home/HeroSlider/Popular/Popular.jsx
@@ -2,38 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Popular.css';
 
-import download from '../../../../assets/2.png';
+import download from '../../../../assets/play-removebg-preview.png';
 const Popular = ({ popular }) => {
     const imgUrl = 'https://image.tmdb.org/t/p/w500';
     return (
         <div>
-            <div className="card shadow-xl border-2 border-green-500">
+            <div className="shadow-xl border-2 border-green-500 overflow-hidden">
                 <div className='relative'>
                     <figure className='rounded-t-[12px] ml-[-1px]'>
-                        <img src={imgUrl + popular?.backdrop_path} alt="Shoes" className='w-full brightness-50' />
+                        <img src={imgUrl + popular?.backdrop_path} alt="Shoes" className='w-full brightness-50 h-full' />
                     </figure>
                     <Link to={`/movie/${popular?.id}`}>
-                        <img src={download} alt="download" className='absolute w-1/4 h-[40%] z-20 top-[40px] left-1/2 translate-x-[-50%]' />
+                        <img src={download} alt="download" className='absolute w-[15%] h-[30%] z-20 top-[50px] left-1/2 translate-x-[-50%]' />
                     </Link>
-                </div>
+                    <Link to={`/movie/${popular?.id}`}>
+                        <div className="info  p-3 absolute top-0 left-0 w-full h-full opacity-0 hover:opacity-100 transition">
+                            <h2 className='font-bold'>{popular?.original_title}</h2>
+                            <span>Rating: {popular?.vote_average}</span>
+                            <p>popularity:{popular?.popularity}</p>
+                            <p>Release Date: {popular.release_date}</p>
+                        </div>
+                    </Link>
 
-                <div className="card-body">
-                    <h2 className="card-title text-xl">
-                        {popular?.original_title}
-                    </h2>
-                    <p>
-                        {
-                            (popular?.overview.length > 120)
-                                ?
-                                <>{popular?.overview.slice(0, 120)} ...</>
-                                :
-                                popular?.overview
-                        }
-                    </p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
                 </div>
             </div>
         </div>
--- src/Shared/Navber/Navber.jsx
+++ src/Shared/Navber/Navber.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navber = () => {
     const [theme, setTheme] = useState("");
@@ -44,7 +45,7 @@ const Navber = () => {
                     </div>
                 </div>
                 <div className="navbar-center">
-                    <a href='/' className="btn btn-ghost normal-case text-xl">MOVIEFLIX</a>
+                    <Link to='/' className="btn btn-ghost normal-case text-xl">MOVIEFLIX</Link>
                 </div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
