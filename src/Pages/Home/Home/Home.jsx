1648b374ffcb5b8c46cb2451008b3a8c50d7c685
reshad21 <miakala7@gmail.com>
2023-02-13 09:17:14 +0600

    upcomming section create

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSlider from '../HeroSlider/HeroSlider';
 import HomeSlider from '../HomeSlider/HomeSlider';
+import Upcoming from '../Upcoming/Upcoming';
 
 const Home = () => {
     return (
         <div className='lg:px-24 md:px-4 px-2 py-7 mt-12 dark:bg-[#3d4451] dark:text-white'>
             <HomeSlider></HomeSlider>
             <HeroSlider></HeroSlider>
+            <Upcoming></Upcoming>
         </div>
     );
 };
---
+++ src/Pages/Home/Upcoming/Upcoming.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Upcoming = () => {
+    return (
+        <div>
+            <h1 className='my-8 font-bold text-2xl border-2 border-green-500 inline-block rounded p-2'>Upcoming MOVIES: <span className='text-green-500'>22</span></h1>
+        </div>
+    );
+};
+
+export default Upcoming;
