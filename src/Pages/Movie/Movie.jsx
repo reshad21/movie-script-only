362d3827b5ddffd5f4d257f7e0623630c9db8f9b
reshad21 <miakala7@gmail.com>
2023-02-15 23:43:46 +0600

    btn large

--- src/Pages/Movie/Movie.jsx
+++ src/Pages/Movie/Movie.jsx
@@ -37,8 +37,8 @@ const Movie = () => {
         <div className='lg:px-24 md:px-4 px-2 py-20 mt-12 dark:bg-[#3d4451] dark:text-white movie'>
             <JwPlayer currentMovieDetail={currentMovieDetail} key={currentMovieDetail?.posterImage}></JwPlayer>
             <div className="action__btn my-5">
-                <button className='btn btn-success mx-2'>Watch now</button>
-                <button className='btn btn-success'>Download now</button>
+                <button className='btn btn-success btn-xl mx-2'>Watch now</button>
+                <button className='btn btn-success btn-xl'>Download</button>
             </div>
         </div>
     );
