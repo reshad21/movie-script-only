b33a12beb520443a5d838cafed1ba781ee27c682
reshad21 <miakala7@gmail.com>
2023-02-15 11:42:05 +0600

    upcomming component customize

--- src/Pages/Home/Upcoming/Upcoming.jsx
+++ src/Pages/Home/Upcoming/Upcoming.jsx
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { FaAngleDoubleRight } from 'react-icons/fa';
-import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useRef } from 'react';
+import { FaAngleDoubleRight, FaRegCaretSquareLeft, FaRegCaretSquareRight } from 'react-icons/fa';
 import Comming from './Comming/Comming';
+
+import { Navigation } from "swiper";
+import 'swiper/css';
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from 'swiper/react';
 const Upcoming = () => {
+    const swiperRef = useRef();
     // const API_KEY = process.env.REACT_APP_apiKey;
     const { data: upcomings = [], isLoading } = useQuery({
         queryKey: ['upcomings'],
@@ -25,13 +29,19 @@ const Upcoming = () => {
         )
     }
     return (
-        <div>
+        <div className='relative'>
             <div className="title flex items-center gap-1 align-middle">
                 <h1 className='my-4 text-2xl inline-block rounded p-2 text-slate-200'>Upcoming Movies</h1>
                 <span className='text-green-500 inline-block text-2xl mt-[5px]'><FaAngleDoubleRight /></span>
             </div>
 
             <Swiper
+                modules={[Navigation]}
+                // navigation={true}
+                // style={{ marginLeft: '40px', marginRight: '40px', position: 'unset' }}
+                onBeforeInit={(swiper) => {
+                    swiperRef.current = swiper;
+                }}
                 spaceBetween={20}
                 slidesPerView={5}
                 breakpoints={{
@@ -61,6 +71,10 @@ const Upcoming = () => {
                     upcomings?.map(upcoming => <SwiperSlide><Comming upcoming={upcoming} key={upcoming?.id}></Comming></SwiperSlide>)
                 }
             </Swiper>
+            <div className='customize__navigation absolute top-7 right-3 text-3xl font-light text-slate-300'>
+                <button onClick={() => swiperRef.current?.slidePrev()}><FaRegCaretSquareLeft /></button>
+                <button onClick={() => swiperRef.current?.slideNext()}><FaRegCaretSquareRight /></button>
+            </div>
         </div>
     );
 };
