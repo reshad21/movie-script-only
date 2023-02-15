2a817d82f47477b1572b8e7765bc38c06745d1c5
reshad21 <miakala7@gmail.com>
2023-02-15 11:56:31 +0600

    today aritime component modified

--- src/Pages/Home/TodayAringShow/TodayAringShow.jsx
+++ src/Pages/Home/TodayAringShow/TodayAringShow.jsx
@@ -1,10 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { FaAngleDoubleRight } from 'react-icons/fa';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useRef } from 'react';
+import { FaAngleDoubleRight, FaRegCaretSquareLeft, FaRegCaretSquareRight } from 'react-icons/fa';
 import AirTime from './AirTime';
 
+import { Navigation } from "swiper";
+import 'swiper/css';
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 const TodayAringShow = () => {
+    const swiperRef = useRef();
     // const API_KEY = process.env.REACT_APP_apiKey;
     const { data: todayShows = [], isLoading } = useQuery({
         queryKey: ['todayShows'],
@@ -25,12 +30,18 @@ const TodayAringShow = () => {
         )
     }
     return (
-        <div>
+        <div className='relative'>
             <div className="title flex items-center gap-1 align-middle">
                 <h1 className='my-4 text-2xl inline-block rounded p-2 text-slate-200'>Today Aring Tv Show</h1>
                 <span className='text-green-500 inline-block text-2xl mt-[5px]'><FaAngleDoubleRight /></span>
             </div>
             <Swiper
+                modules={[Navigation]}
+                // navigation={true}
+                // style={{ marginLeft: '40px', marginRight: '40px', position: 'unset' }}
+                onBeforeInit={(swiper) => {
+                    swiperRef.current = swiper;
+                }}
                 spaceBetween={20}
                 slidesPerView={5}
                 breakpoints={{
@@ -61,6 +72,10 @@ const TodayAringShow = () => {
                     todayShows?.map(todayShow => <SwiperSlide><AirTime todayShow={todayShow} key={todayShow?.id}></AirTime></SwiperSlide>)
                 }
             </Swiper>
+            <div className='customize__navigation absolute top-7 right-3 text-3xl font-light text-slate-300'>
+                <button onClick={() => swiperRef.current?.slidePrev()}><FaRegCaretSquareLeft /></button>
+                <button onClick={() => swiperRef.current?.slideNext()}><FaRegCaretSquareRight /></button>
+            </div>
         </div>
     );
 };
