0b2d92c0612f7cc2fd5a58de4d5badd3898768c4
reshad21 <miakala7@gmail.com>
2023-02-16 16:25:03 +0600

    navber change

--- src/Shared/Navber/Navber.jsx
+++ src/Shared/Navber/Navber.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navber.css';
 import SearchItem from './SearchItem';
 
 const Navber = () => {
     const [theme, setTheme] = useState("");
-
     useEffect(() => {
         if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
             // setTheme('dark');
@@ -27,28 +27,79 @@ const Navber = () => {
     const handleThemeSwitch = () => {
         setTheme(theme === "dark" ? "light" : "dark");
     };
+
+
+    const [toggle, setToggle] = useState(false);
+    const handleToggle = () => {
+        (toggle === false) ? setToggle(true) : setToggle(false);
+    }
     const menuItems = <>
-        <li><Link to='/'>Homepage</Link></li>
-        <li><Link to='/allMovies'>Movies</Link></li>
-        <li><Link to='/allTvShows'>Tv Show</Link></li>
+        <li>
+            <Link className='p-0 mr-3 bg-inherit' to='/'>
+                Home
+            </Link>
+        </li>
+        <li tabIndex={0} >
+            <Link className="p-0 mr-3 justify-between bg-inherit" to='/allMovies'>
+                Movies
+            </Link>
+        </li>
+        <li>
+            <Link className='p-0 m-0 bg-inherit' to='/allTvShows'>
+                Tv Show
+            </Link>
+        </li>
+    </>
+    const dropdownItems = <>
+        <div className="flex flex-col gap-1">
+            <li>
+                <Link className='p-0 bg-inherit' to='/'>
+                    <button className='btn btn-outline w-full border-orange-500 font-bold text-orange-500'>Home</button>
+                </Link>
+            </li>
+            <li tabIndex={0} >
+                <Link className="p-0 justify-between bg-inherit" to='/allMovies'>
+                    <button className='btn btn-outline w-full border-orange-500 font-bold text-orange-500'>Movies</button>
+                </Link>
+            </li>
+            <li>
+                <Link className='p-0 bg-inherit' to='/allTvShows'>
+                    <button className='btn btn-outline w-full border-orange-500 font-bold text-orange-500'>Tv Show</button>
+                </Link>
+            </li>
+            <li>
+                <SearchItem></SearchItem>
+            </li>
+
+
+
+        </div>
     </>
     return (
         <div>
             <div className="navbar bg-base-100 dark:bg-[#2f3541] dark:text-white fixed top-0 z-30 w-full lg:px-[60px] md:px-[30px] backdrop-blur">
-                <div className="navbar-start">
-                    <ul className="menu menu-vertical lg:menu-horizontal text-success">
-                        {
-                            menuItems
-                        }
-                    </ul>
-                </div>
-                {/* <div className="navbar-center">
-                    <Link to='/' className="btn btn-ghost normal-case text-xl">MOVIEFLIX</Link>
-                </div> */}
-                <div className="navbar-end">
-                    <SearchItem></SearchItem>
+                <div className="navbar justify-between ">
+                    <div className="w-full flex justify-between">
+                        <ul className="menu menu-horizontal px-1 hidden lg:flex">
+                            {menuItems}
+                        </ul>
 
-                    <button className='btn btn-outline btn-sm dark:bg-[#3d4451] dark:text-white' onClick={handleThemeSwitch}>Mode</button>
+                        <Link to='/' className='logo'><button className='btn btn-success btn-outline'>Home</button></Link>
+
+                        <div className="dropdown">
+                            <Link onClick={handleToggle} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-slate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </Link>
+                            <ul className={toggle ? 'block absolute z-50 right-0 w-52 menu menu-compact mt-3 p-2 shadow bg-[#000000a6] rounded-box ' : 'hidden'}>
+                                {dropdownItems}
+                                <button className='btn btn-outline btn-sm dark:bg-[#3d4451] dark:text-white mt-2 py-2' onClick={handleThemeSwitch}>Mode</button>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <SearchItem></SearchItem>
+                        <button className='btn btn-outline btn-sm dark:bg-[#3d4451] dark:text-white ml-2 py-2' onClick={handleThemeSwitch}>Mode</button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Shared/Navber/SearchItem.jsx
+++ src/Shared/Navber/SearchItem.jsx
@@ -1,51 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import React from 'react';
 
 const SearchItem = () => {
-    const [inputValue, setInputValue] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const name = form.name.value;
-        console.log(name);
-        setInputValue(name);
-        console.log("updatted value is:", inputValue);
-    }
-
-    const { data: searchValue = [], isLoading } = useQuery({
-        queryKey: ['searchValue', inputValue],
-        queryFn: async () => {
-            const res = await fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`);
-            const data = await res.json();
-            return data.results;
-        }
-    })
-
-    // console.log(inputValue);
-    console.log(searchValue);
-
-    if (isLoading) {
-        return (
-            <div className='bg-white flex items-end justify-center h-[200px]'>
-                <h1 className='text-2xl font-semibold text-slate-600'>Loading...</h1>
-            </div>
-        )
-    }
 
 
 
     return (
-        <div>
-            <form className="" onSubmit={handleSubmit}>
+        <div className='m-0 p-0'>
+            <form className="block w-[100%]">
+                <div className="relative w-full">
+                    <input type="text" name='name' placeholder="Search..." className="input bg-inherit border border-slate-400 w-[192px] md:w-full sm:full" />
 
-                {/* <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Search..." className="input bg-inherit border border-slate-400" /> */}
+                    <button className="btn btn-ghost btn-circle absolute top-0 right-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                </div>
 
-                <input type="text" name='name' placeholder="Search..." className="input bg-inherit border border-slate-400" />
-
-                <button className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                </button>
             </form>
         </div>
     );
