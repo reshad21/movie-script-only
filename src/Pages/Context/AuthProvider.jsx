acf293d732a8e8eeb7f137b39f655d1b62afde5c
reshad21 <miakala7@gmail.com>
2023-02-25 23:19:48 +0600

    multiple search api findout from tmdb

--- src/Pages/Context/AuthProvider.jsx
+++ src/Pages/Context/AuthProvider.jsx
@@ -4,6 +4,7 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+    // https://api.themoviedb.org/3/search/multi?api_key=60328c60edaea9ec7115178b6e8c7a3a&query=${searchTerm}
     const [data, setData] = useState([]);
     const collections = (searchTerm) => {
         fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
--- src/Shared/Navber/ShowSearch/ShowSearch.jsx
+++ src/Shared/Navber/ShowSearch/ShowSearch.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { FaYoutube } from 'react-icons/fa';
+import { FaStar, FaYoutube } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import download from '../../../assets/play-removebg-preview.png';
 import { AuthContext } from '../../../Pages/Context/AuthProvider';
@@ -19,21 +19,28 @@ const ShowSearch = () => {
                                 <div className="shadow-xl border-2 rounded-[6px] border-slate-700 overflow-hidden">
                                     <div className='relative'>
                                         <figure className='rounded-t-[12px] ml-[-1px]'>
-                                            <img src={download} alt="Shoes" className='w-full h-[300px] object-cover' />
+                                            <img src={(item.show.image?.medium) ? (item.show.image?.medium) : download} alt="Shoes" className='w-full h-[300px] object-cover' />
                                         </figure>
 
                                         <Link to={`/movie/${item?.show?.id}`}>
                                             <div className="info p-3 absolute top-0 left-0 w-full h-full opacity-0 hover:opacity-100 transition text-slate-200 bg-[#0000005b]">
-                                                {/* <span>Rating: {item?.rating?.average} <FaStar className='star inline-block mb-1 text-yellow-600' /></span> */}
-                                                {/* <p>popularity: {popular?.popularity}</p> */}
-                                                <p>Release Date: {item?.premiered}</p>
+                                                <span>Rating: {item?.show?.rating?.average} <FaStar className='star inline-block mb-1 text-yellow-600' /></span>
+                                                <p>Category : {
+                                                    (item?.show?.genres[0]) || (item?.show?.genres[1])
+                                                        ?
+                                                        <span>{item?.show?.genres[0]}, {item?.show?.genres[1]}</span>
+                                                        :
+                                                        item?.show?.genres[0]
+
+                                                }</p>
+                                                <p>Release Date: {item?.show?.premiered}</p>
                                                 <FaYoutube className='inline-block text-7xl ml-20 mt-9 text-rose-500' />
                                             </div>
                                         </Link>
 
                                     </div>
                                 </div>
-                                <p className='pl-2 pt-1 font-extralight text-[14px] text-slate-600 dark:text-slate-200'>{item?.name}</p>
+                                <p className='pl-2 pt-1 font-extralight text-[14px] text-slate-600 dark:text-slate-200'>{item?.show?.name}</p>
                             </div>
                         )
                     })
