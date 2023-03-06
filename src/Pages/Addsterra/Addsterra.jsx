caa8902aae011f3d743996da0fba2af3e90ce313
reshad21 <miakala7@gmail.com>
2023-03-06 22:57:03 +0600

    project is ready for sell

--- src/Pages/Addsterra/Addsterra.jsx
+++ src/Pages/Addsterra/Addsterra.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 
 const Addsterra = () => {
 
@@ -6,6 +7,7 @@ const Addsterra = () => {
     return (
         <>
             <h1 className='my-4 text-2xl inline-block rounded p-2 text-slate-600 dark:text-slate-200'>Popular Offer</h1>
+            <span className='text-green-500 inline-block text-2xl mt-[5px]'><FaAngleDoubleRight /></span>
 
             <div>
                 <div id="container-d5a75a25fa98fe83c3e89c3614752d1b"></div>
