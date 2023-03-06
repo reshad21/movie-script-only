f87bfb6ec544c0c3c55730cbb631ae97ac5393d7
reshad21 <miakala7@gmail.com>
2023-03-06 14:11:51 +0600

    nativbar add

---
+++ src/Pages/Addsterra/Addsterra.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Addsterra = () => {
+
+    <script async="async" data-cfasync="false" src="//pl18597852.highrevenuecpmnetwork.com/d5a75a25fa98fe83c3e89c3614752d1b/invoke.js"></script>
+    return (
+        <div>
+            <div id="container-d5a75a25fa98fe83c3e89c3614752d1b"></div>
+        </div>
+    );
+};
+
+export default Addsterra;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Addsterra from '../../Addsterra/Addsterra';
 import DIvider from '../DIvider/DIvider';
 import Populers from '../HeroSlider/Populers';
 import HomeSlider from '../HomeSlider/HomeSlider';
@@ -19,6 +20,7 @@ const Home = () => {
             <PopularTvShow></PopularTvShow>
             <TodayAringShow></TodayAringShow>
             <TopRatedShow></TopRatedShow>
+            <Addsterra></Addsterra>
         </div>
     );
 };
