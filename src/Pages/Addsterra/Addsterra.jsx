510703db6056feb1be31f9c44023043f61e21463
reshad21 <miakala7@gmail.com>
2023-03-10 23:34:43 +0600

    useeffect using for adstera code implementation

--- src/Pages/Addsterra/Addsterra.jsx
+++ src/Pages/Addsterra/Addsterra.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
 
 const Addsterra = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = '//pl18597852.highrevenuecpmnetwork.com/d5a75a25fa98fe83c3e89c3614752d1b/invoke.js';
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []);
 
-    <script async="async" data-cfasync="false" src="//pl18597852.highrevenuecpmnetwork.com/d5a75a25fa98fe83c3e89c3614752d1b/invoke.js"></script>
     return (
         <>
             <h1 className='my-4 text-2xl inline-block rounded p-2 text-slate-600 dark:text-slate-200'>Popular Offer</h1>
