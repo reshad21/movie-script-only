f3fb4a58e601d55f28182f4b07d6f3242f312431
reshad21 <miakala7@gmail.com>
2023-03-15 00:52:25 +0600

    single movie data show

--- src/Pages/Movie/Movie.jsx
+++ src/Pages/Movie/Movie.jsx
@@ -13,15 +13,13 @@ const Movie = () => {
     const { data: currentMovieDetail = [], isLoading } = useQuery({
         queryKey: ['currentMovieDetail', id],
         queryFn: async () => {
-
-
             const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`);
             const data = await res.json();
             return data;
         }
     })
 
-    // console.log(currentMovieDetail);
+    console.log(currentMovieDetail);
 
     const imgUrl = 'https://image.tmdb.org/t/p/w500';
 
@@ -43,6 +41,51 @@ const Movie = () => {
             <div className="action__btn my-5">
                 <button className='btn btn-success btn-xl mx-2'>Watch now</button>
                 <button className='btn btn-success btn-xl'>Download</button>
+            </div>
+            <div className="flex gap-3 bg-slate-900 p-3 rounded-lg">
+                <div className=''>
+                    <img src={posterImage} alt="" title='' />
+                </div>
+
+                <div className=''>
+                    <div className='relative'>
+                        <h1 className='text-[1.8rem]'>{currentMovieDetail?.original_title} <span className='text-[#8e9aa5] text-[50%]'>{currentMovieDetail?.release_date.slice(0, 4)}</span></h1>
+                        <div className='border-2 border-green-500 absolute top-0 right-0 p-3 rounded w-[25%] text-center'>
+                            <a href="https://everydaymovie.online/en/loading?id=315162&amp;title=Puss%20in%20Boots%3A%20The%20Last%20Wish">Subscribe to Watch | $0.00</a>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <i className="fa fa-star text-warning"></i>
+                            <i className="fa fa-star text-warning"></i>
+                            <i className="fa fa-star text-warning"></i>
+                            <i className="fa fa-star text-warning"></i>
+                            <i className="fa fa-star text-warning"></i>
+                            <i className="fa fa-star text-warning"></i>
+                            <i className="fa fa-star text-warning"></i>
+                            <i className="fa fa-star text-warning"></i>
+                            <i className="fa fa-star-o text-muted"></i>
+                            <i className="fa fa-star-o text-muted"></i>
+                        </div>
+                        <div>
+                            <span>{currentMovieDetail?.vote_average}/10</span> <span>by {currentMovieDetail?.vote_count} users</span>
+                        </div>
+                    </div>
+                    <div>
+                        <p>{currentMovieDetail?.overview}</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Released: <span>{currentMovieDetail?.release_date}</span></li>
+                            <li>Runtime: <span>{currentMovieDetail?.runtime} minutes</span></li>
+
+                            <li>Genre: {currentMovieDetail?.genres.map(genre => <span>{genre.name}, </span>)}</li>
+
+
+                            <li>production Companies: {currentMovieDetail?.production_companies.map(production => <span>{production?.name}, </span>)}</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <Iframe url="https://www.youtube.com/embed/MBOhKd6cIak"
                 width="640px"
